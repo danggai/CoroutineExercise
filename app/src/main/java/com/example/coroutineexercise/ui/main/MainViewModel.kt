cca602a872ae5429bc08b180464d05dbe4b772f3
danggai <donggi9313@naver.com>
2021-05-04 17:01:45 +0900

    - 기본 코루틴 함수 구현

--- app/src/main/java/com/example/coroutineexercise/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/coroutineexercise/ui/main/MainViewModel.kt
@@ -1,7 +1,41 @@
 package com.example.coroutineexercise.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    init {
+        launchAll()
+    }
+
+    private fun launchAll() = runBlocking {
+        Log.e("All", "Start")
+        launchA()
+        Log.e("A", "Done")
+        launchB()
+        Log.e("B", "Done")
+        launchC()
+        Log.e("C", "Done")
+    }
+
+    suspend fun launchA() {
+        Log.e("A", "Start")
+        delay(500L)
+        Log.e("A", "Completing")
+    }
+
+    suspend fun launchB() {
+        Log.e("B", "Start")
+        delay(500L)
+        Log.e("B", "Completing")
+    }
+
+    suspend fun launchC() {
+        Log.e("C", "Start")
+        delay(500L)
+        Log.e("C", "Completing")
+    }
 }
